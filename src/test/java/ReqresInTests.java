c2ab73909ad436239d97115be95b7519734ed65f
elenachaplygina <antonenko-elena@inbox.ru>
2023-03-20 19:32:26 +0300

    added steps

--- src/test/java/ReqresInTests.java
+++ src/test/java/ReqresInTests.java
@@ -1,8 +1,8 @@
-import io.restassured.filter.Filter;
 import model.UserBodyRequestModel;
 import model.UserBodyResponseModel;
 import org.junit.jupiter.api.Test;
 
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 import static specs.UserSpecs.userRequestSpec;
@@ -16,53 +16,64 @@ public class ReqresInTests extends TestBase{
         UserBodyRequestModel data=new UserBodyRequestModel();
         data.setName("Homer Simpson");
         data.setJob("nuclear safety inspector");
-        UserBodyResponseModel response = given(userRequestSpec)
+        UserBodyResponseModel response =
+                step("Get name and job data",()->
+                given(userRequestSpec)
                 .body(data)
                 .when()
                 .patch("/users/2")
                 .then()
                 .spec(userResponseSpec)
-                .extract().as(UserBodyResponseModel.class);
-        assertThat(response.getName()).isEqualTo("Homer Simpson");
-        assertThat(response.getJob()).isEqualTo("nuclear safety inspector");
+                .extract().as(UserBodyResponseModel.class));
+        step("Verify update response name",()->
+        assertThat(response.getName()).isEqualTo("Homer Simpson"));
+        step("Verify update job",()->
+        assertThat(response.getJob()).isEqualTo("nuclear safety inspector"));
     }
 
     @Test
     void patchOnlyNameTest() {
         UserBodyRequestModel data=new UserBodyRequestModel();
         data.setName("Homer Simpson");
-        UserBodyResponseModel response = given(userRequestSpec)
+        UserBodyResponseModel response =
+                step("Get only name data",()->
+                        given(userRequestSpec)
                 .body(data)
                 .when()
                 .patch("/users/2")
                 .then()
                 .spec(userResponseSpec)
-                .extract().as(UserBodyResponseModel.class);
-        assertThat(response.getName()).isEqualTo("Homer Simpson");
+                .extract().as(UserBodyResponseModel.class));
+        step("Verify update name",()->
+                assertThat(response.getName()).isEqualTo("Homer Simpson"));
     }
 
     @Test
     void patchOnlyJobTest() {
         UserBodyRequestModel data=new UserBodyRequestModel();
         data.setJob("nuclear safety inspector");
-        UserBodyResponseModel response = given(userRequestSpec)
+        UserBodyResponseModel response =
+                step("Get only job data",()->
+                        given(userRequestSpec)
                 .body(data)
                 .when()
                 .patch("/users/2")
                 .then()
                 .spec(userResponseSpec)
-                .extract().as(UserBodyResponseModel.class);
-        assertThat(response.getJob()).isEqualTo("nuclear safety inspector");
+                .extract().as(UserBodyResponseModel.class));
+        step("Verify update job",()->
+                assertThat(response.getJob()).isEqualTo("nuclear safety inspector"));
 
     }
 
     @Test
     void patchWithountBodyTest() {
-        given(userRequestSpec)
+        step("Verify status code withount request body",()->
+                given(userRequestSpec)
                 .when()
                 .patch("/users/2")
                 .then()
-                .spec(userResponseSpec);
+                .spec(userResponseSpec));
     }
 
     @Test
@@ -71,16 +82,21 @@ public class ReqresInTests extends TestBase{
         data.setName("Homer Simpson");
         data.setJob("nuclear safety inspector");
         data.setWife("Marge Simpson");
-        UserBodyResponseModel response = given(userRequestSpec)
+        UserBodyResponseModel response =
+                step("Get name, job and wife data",()->
+                        given(userRequestSpec)
                 .body(data)
                 .when()
                 .patch("/users/2")
                 .then()
                 .spec(userResponseSpec)
-                .extract().as(UserBodyResponseModel.class);
-        assertThat(response.getJob()).isEqualTo("nuclear safety inspector");
-        assertThat(response.getName()).isEqualTo("Homer Simpson");
-        assertThat(response.getWife()).isEqualTo("Marge Simpson");
+                .extract().as(UserBodyResponseModel.class));
+        step("Verify update job",()->
+                assertThat(response.getJob()).isEqualTo("nuclear safety inspector"));
+        step("Verify update name",()->
+                assertThat(response.getName()).isEqualTo("Homer Simpson"));
+        step("Verify update wife",()->
+                assertThat(response.getWife()).isEqualTo("Marge Simpson"));
 
     }
 
